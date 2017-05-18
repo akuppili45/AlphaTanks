5f84f3881aabf22630beb9e04df4136dfeef542d
christopheroboekim <christopher.kim.365@gmail.com>
2017-05-18 12:06:18 -0700

    Added instance variable

--- src/com/company/Gun.java
+++ src/com/company/Gun.java
@@ -12,6 +12,7 @@ public class Gun extends GameObject
     Stack<Bullet> bullets;
     private float angle;
     private Tank tankID;
+    int x;
     public Gun(float x, float y, ObjectId id, float gunAngle, Tank tank) {
         super(x, y, id);
         this.bullets = new Stack<Bullet>();
